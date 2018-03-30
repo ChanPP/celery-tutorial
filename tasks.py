9e66f50d2ab1f6f9de1050c228a7442ef2e6af28
park chan <parkchan428@gmail.com>
2018-03-30 15:56:05 +0900

    django 연동

--- tasks.py
+++ tasks.py
@@ -1,8 +1,10 @@
 import time
 from celery import Celery
 
-app = Celery('tasks', broker='redis://localhost:6379/0',
-             backend='redis://localhost:6379/0')
+app = Celery('tasks',
+             broker='redis://localhost:6379/0',
+             backend='redis://localhost:6379/0',
+             )
 
 
 @app.task
